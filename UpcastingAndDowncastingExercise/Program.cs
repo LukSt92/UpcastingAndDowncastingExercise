fd6c44cc5318b25c67ade5d2ed89e00e168a3ef3
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-12 14:41:55 +0200

    Add project files.

---
+++ UpcastingAndDowncastingExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingAndDowncastingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
